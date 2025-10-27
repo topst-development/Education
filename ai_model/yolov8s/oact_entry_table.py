dc69917fa70dd86441c05da1232455db33f25b48
TOPST Dev. <161264431+Topst-Dev@users.noreply.github.com>
2025-10-27 10:12:19 +0900

    Delete ai_model/yolov8s directory

--- ai_model/yolov8s/oact_entry_table.py
+++
@@ -1,8 +0,0 @@
-
-oacts = list()
-oacts.append([       0x0,    0x64000,  4,         64,          0, 'Conv2d_41'])
-oacts.append([   0x64000,    0x32000,  4,          8,          0, 'Conv2d_42'])
-oacts.append([   0x96000,    0x19000,  4,         64,          0, 'Conv2d_52'])
-oacts.append([   0xaf000,     0xc800,  4,          8,          0, 'Conv2d_53'])
-oacts.append([   0xbb800,     0x6400,  4,         64,          0, 'Conv2d_61'])
-oacts.append([   0xc1c00,     0x3200,  4,          8,          0, 'Conv2d_62'])
